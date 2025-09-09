421d19f9b7fb957b14e5d72eb3bd3d80368b4fd7
George <jorgenriquemartinezc@gmail.com>
2025-09-08 23:03:03 -0500

    cambio para que funcione en dispositivos moviles

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,3 @@
 // Contenido para: frontend/config.js
-const API_BASE_URL = 'https://certificadosloto-app.onrender.com';
+// frontend/config.js
+const API_BASE_URL = 'https://certificadosloto-app.onrender.com/api';
